bd1551633429879ee3686940e16ceeac2461381d
Alex <alexis.mar.schmidt@gmail.com>
2024-11-27 12:07:54 +0100

    change backend message

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.json("All good in here");
+  res.json("Green Backend is live!!!");
 });
 
 module.exports = router;
